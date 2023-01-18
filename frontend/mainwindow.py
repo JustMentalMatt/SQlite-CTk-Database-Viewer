9eae46757c36e00290dfc939e4977b014d1ccae8
JustMentalMatt <matthoey69@hotmail.com>
2023-01-18 22:31:26 +0000

    Added new frames
    title, usrinfo and button box frames added

--- frontend/mainwindow.py
+++ frontend/mainwindow.py
@@ -7,30 +7,48 @@ def mainWindow():
 
     root = customtkinter.CTk()
     root.geometry("1280x720")
-
-
-    frame = customtkinter.CTkFrame(master=root)
-    frame.pack(pady=20, padx=60, fill="both", expand=True)
-
-    tableText = customtkinter.CTkLabel(master=frame, text="Browse Tables:", font=("Roboto", 24))
-    tableText.pack(side="left", pady=0, padx=10)
-    
-    grid = customtkinter.CTkFrame(master=frame)
-    grid.pack(pady=300, padx=60, fill="both", expand=False)
-        
-    
-
-    def optionmenu_callback(choice):
-        print("optionmenu dropdown clicked:", choice)
-
-    combobox = customtkinter.CTkOptionMenu(master=root,
-                                       values=["Admin_Users", "Guest_Users"],
-                                       command=optionmenu_callback)
-    combobox.pack(padx=20, pady=10)
-    combobox.set("TABLES")  # set initial value
-    
     root.title("Database Viewer")
     root.resizable(False,False)
+    
+                                            # --------- Frames --------- #:
+    
+    # Title Box:
+    titleboxFrame = customtkinter.CTkFrame(master=root)
+    titleboxFrame.grid(column=0, columnspan=2, row=0, rowspan=1, ipadx=5, ipady=5, padx=20, pady=20, sticky="nsew")
+    tb_title = customtkinter.CTkLabel(master=titleboxFrame, text="SQLite Database Viewer", font=("Roboto", 24, "underline",))
+    tb_title.grid(column=0, row=0, ipadx=10, ipady=10, sticky="nsew")
+    tb_text = customtkinter.CTkLabel(master=titleboxFrame, text=" Database Viewer V1    ~    Author: Ma 💕", font=("Roboto", 12))
+    tb_text.grid(column=0, columnspan=2, row=1, ipadx=10, sticky="w")
+    
+    # User Info Box:
+    usrinfoFrame = customtkinter.CTkFrame(master=root)
+    usrinfoFrame.grid(column=0, columnspan=2, row=2, rowspan=1, ipadx=5, ipady=5, padx=20, pady=10, sticky="nsew")
+    usrName = customtkinter.CTkLabel(master=usrinfoFrame, text="¬ Logged in as: ", font=("Roboto", 14))
+    usrName.grid(column=0, row=0, ipadx=10, ipady=10, sticky="SW")
+    usrAuth = customtkinter.CTkLabel(master=usrinfoFrame, text="¬ Auth Level:  ", font=("Roboto", 14))
+    usrAuth.grid(column=0, row=2, ipadx=10, sticky="SW")
+    
+    # Button Box:
+    btnboxFrame = customtkinter.CTkFrame(master=root)
+    btnboxFrame.grid(column=0, columnspan=2, row=0, rowspan=10, ipadx=20, ipady=20, padx=20, pady=20, sticky="nsew")
+    
+
+#########################################################################################################################
+
+
+    # tableText = customtkinter.CTkLabel(master=btnboxFrame, text="Browse Tables:", font=("Roboto", 24))
+    # tableText.grid(column=0, row=0, pady=(20, 0), sticky="nsew")
+    
+    # def optionmenu_callback(choice):
+    #     print("optionmenu dropdown clicked:", choice)
+
+    # combobox = customtkinter.CTkOptionMenu(master=frame, values=["Admin_Users", "Guest_Users"], command=optionmenu_callback) 
+                                          
+                                                                         
+    # combobox.grid(row=1, column=1)
+    # combobox.set("TABLES")  # set initial value
+    
+    
     root.mainloop()
     
 mainWindow()
